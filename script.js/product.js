5d96dea17614012ffeb5e76475a704c849d575e7
LKONATE-c <76518534+LKONATE-c@users.noreply.github.com>
2021-06-11 11:24:56 +0200

    men

--- script.js/product.js
+++ script.js/product.js
@@ -17,7 +17,7 @@ fetch(`http://localhost:3000/api/teddies/${id}`)
     // Une erreur est survenue
   });
   function displayProduct(product){
-      console.log("rrrrr")
+      
     
       
      
@@ -31,14 +31,34 @@ fetch(`http://localhost:3000/api/teddies/${id}`)
           <div class="card">
               <img class=”card-img-top” src="${product.imageUrl}" alt="">
               <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <p class="card-price">Prix:&nbsp;${product.price / 100} &#8364;</p>
-                <p class="card-text">Description:${product.description}</p>
-                <a href="products.html?id=${product._id}" class="stretched-link"></a>
-              </div>
-          </div>
-      </div>`
+                    <h5 class="card-title">${product.name}</h5>
+                    <p class="card-price">Prix:&nbsp;${product.price / 100} &#8364;</p>
+                    <p class="card-text">Description:${product.description}</p>
+                    <a href="products.html?id=${product._id}" class="stretched-link"></a>
+                    <p class="card-colors">colors${product.colors}</p>
+
+                </div>
+
+                   
+            </div>
+        </div>`
+        getproduct().then(data=> {
+            let option;
+            Object.entries(data.colors).forEach(color => {
+              
+                option = document.createElement("option");
+                option.text =colors.name;
+
+                colorSelect.add(option)
+            }
+                )
+        }
+            )
+        
+        
      }
+
+
     
 
 
