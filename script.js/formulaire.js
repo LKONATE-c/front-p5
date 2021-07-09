caf469878579bdb3c94019bd3af261ba88a5177a
LKONATE-c <76518534+LKONATE-c@users.noreply.github.com>
2021-07-09 09:47:15 +0200

    formu

--- script.js/formulaire.js
+++ script.js/formulaire.js
@@ -1,15 +1,15 @@
 
 //validation du formulaire
-document.forms["coordonnee"].addEventListener("submit", function(e) {
+document.getElementById("coordonnee").addEventListener("submit", function(e) {
 
     let erreur;
 
-    let inputs = this;
+    let inputs = this.getElementsByTagName("input");
 
     for (let i = 0; i < inputs.length; i++) {
         if (!inputs[i].value) {
             erreur = "veuillez renseigner tous les champs";
-            break;
+           
         }
     }
 
@@ -18,14 +18,30 @@ document.forms["coordonnee"].addEventListener("submit", function(e) {
         document.getElementById("erreur").innerHTML = erreur;
         return false;
     } else {
+
+        let firstName  = document.getElementById("firstname").value;
+        let Lastname = document.getElementById("lastname").value;
+        let email = document.getElementById("email").value;
+        let adress = document.getElementById("adress").value;
+        let ville = document.getElementById("city").value;
         
-            fetch("http://127.0.0.1:5500/page-confirmation.html", {
+            fetch("", {
               method: "POST",
               headers: {
                 'Accept': 'application/json', 
                 'Content-Type': 'application/json ; charset="UTF-8'
               },
-              body: JSON.stringify({value: document.getElementsByTagName("coordonnee").value})
+              body: JSON.stringify({
+                  coordonnee: {
+                      firstName:firstname,
+                      Lastname:lastname,
+                      email:email,
+                      adress:adress,
+                      ville:city
+
+
+                  }
+              })
               
             })
             .then(function(res) {
@@ -33,10 +49,10 @@ document.forms["coordonnee"].addEventListener("submit", function(e) {
                   return res.json();
                 }
               })
-              .then(function(value) {
-                document.getElementById("totat-price")
+              .then(function(data) {
+                //document.getElementById("totat-price")
 
-                window.location.assign("/page-confirmation.html");
+                //window.location.assign("");
               });
             
         
