3ed7a26cd1d6f93571e5669f4f17b0bef7149f84
LKONATE-c <76518534+LKONATE-c@users.noreply.github.com>
2021-07-16 18:13:49 +0200

    modification du tableau panier

--- script.js/panier.js
+++ script.js/panier.js
@@ -9,13 +9,14 @@ function getTeddie(){
     
     for (const [name,teddie] of Object.entries(teddies)){
         bear.push (teddie.id);
+         
         document.getElementById("products").innerHTML +=
         `
         <tr>
         <td >${teddie.name}</td>
         <td>${teddie.price} &#8364;</td>
         <td>${teddie.incart}</td>
-        
+        <td>${teddie.price*teddie.incart} &#8364;</td>
         </tr>
         
         `
@@ -27,7 +28,9 @@ function getTeddie(){
 //recuperation du prix total ds le local storage 
 function getprice(){
     const price = JSON.parse(localStorage.getItem("totalCost"))
-    if (price === null) {
+    
+    if (price === null ) {
+        console.log("pppprr");
         return 0;
     }
     
@@ -35,8 +38,9 @@ function getprice(){
     ` 
     <div id="totat-price">${price} &#8364;</div>
     `
-
+    
 }
+
 getprice();
 getTeddie();
 
